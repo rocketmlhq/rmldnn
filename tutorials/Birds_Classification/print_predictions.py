f1dc5fe416d18ea414e182658323ebd5b78e1e19
Yash Jain <91678870+yashjain-99@users.noreply.github.com>
2022-06-17 16:04:08 +0530

    Delete tutorials/Birds_Classification directory

--- tutorials/Birds_Classification/print_predictions.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-import os
-for file in sorted(os.listdir('./predictions/')):
-    print(np.argmax(np.load('./predictions/' + file)), end=' ')
--- tutorials/Birds_Classification/test_sample.py
+++
@@ -1,9 +0,0 @@
-import os
-import shutil
-import random
-os.mkdir('test_sample')
-src='./data/test/'
-dest='./test_sample/'
-for directory in os.listdir(src):
-    random_file=random.choice(os.listdir(src+directory))
-    shutil.copy(src+directory+'/'+random_file,dest)
